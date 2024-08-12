7b0bd23bd2e308f13b7fb4a3ee2d24cbca949b7d
Kristen <kristen.cutler@stitchfix.com>
2024-08-11 19:29:19 -0700

    Add logic and styling for tiles

--- src/ClinicDashboard.js
+++ src/ClinicDashboard.js
@@ -1,5 +1,9 @@
 import "./dashboardStyles.css";
 import { clinicData } from "./clinicData.js";
+import calendarIcon from './assets/calendar-booked-dark-blue.svg';
+import availabilityIcon from './assets/calendar-dark-blue.svg';
+import detailIcon from './assets/right-round-dark-blue.svg';
+import manageIcon from './assets/settings-dark-blue.svg';
 
 function ClinicDashboard() {
   return (
@@ -9,9 +13,33 @@ function ClinicDashboard() {
       </div>
 
       <div className="dashboard-content">
-        {/* your code goes here */}
-        {/* add, remove, or update anything – does not have to be limited 
-          to this section, if there are other changes you want to make */}
+        <div className="tile-container">
+          {clinicData.map((clinic, index) => (
+            <div className="tile" key={index} style={{borderBottom: `30px solid ${clinic.color}`}}>
+              <div className="tile-header">
+                <div className="tile-header-left">Workit @ {clinic.name}</div>
+                <div className="tile-header-right"> {clinic.members} Members | {clinic.providers} Providers </div>
+              </div>
+              <div className="tile-header-border"/>
+              <a href="#" className="tile-row">
+                <img src={detailIcon}/>
+                <span>Details</span>
+              </a>              
+              <a href="#" className="tile-row">
+                <img src={manageIcon}/>
+                <span>Manage</span>
+              </a>
+              <a href="#" className="tile-row">
+                <img src={availabilityIcon}/>
+                <span>Availabilities</span>
+              </a>
+              <a href="#" className="tile-row">
+                <img src={calendarIcon}/>
+                <span>Calendar</span>
+              </a>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
